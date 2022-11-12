5ca44c0816cae956b2c03d2a92eac67506d143fb
Nakano Miku <56882049+iceice666@users.noreply.github.com>
2022-11-12 12:43:31 +0800

    stash

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import block from "./components/Block.vue";
+import terminal from "./components/Termianl.vue";
 </script>
 
 <template>
   <div>
-
+    <div id="blockarea">
+      <terminal msg="" />
+    </div>
   </div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
